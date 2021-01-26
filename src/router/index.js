a703b78f0d174fa69069b80fe48e4bb2c453c2f5
Заур <zaurmag411@gmail.com>
2021-01-26 10:16:36 +0300

    + Сделал первичную настройку приложения: компоненты + роутинг

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Tasks from '@/views/Tasks'
+import New from '@/views/New'
+import Task from '@/views/Task'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Tasks',
+    component: Tasks
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/new',
+    name: 'New',
+    component: New
+  },
+  {
+    path: '/task',
+    name: 'Task',
+    component: Task
   }
 ]
 
